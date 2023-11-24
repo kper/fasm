8fb7109016095456d6f61bef34fa4b889d2c164f
Kevin Per <kevin.per@protonmail.com>
2023-11-24 08:55:26 +0100

    Fixing code parsing

--- src/code.fs
+++ src/code.fs
@@ -2,21 +2,19 @@ require leb128.fs
 require section.fs
 
 \ 5.4.5 Numeric Instructions
-0x41 constant i32.const
+$41 constant i32.const
 \ 0x42 constant i64.const
 
-0x6A constant i32.add
+$6A constant i32.add
 
 : wasm-compile-i32.const ( addr1 -- addr2 )
   char+       \ Forward to constant value.
   dup i32@    \ Read value.
   . cr        \ TODO: Write to file.
-  char+
 ;
 
 : wasm-compile-i32.add ( addr1 -- addr2 )
   s" add" type cr          \ TODO: Write to file.
-  char+
 ;
 
 : wasm-compile-code-section ( addr1 -- addr2 )
@@ -27,7 +25,13 @@ require section.fs
   char+           \ Forward to code block size.
   dup u32@        \ Read size field.
   drop            \ We are not intersted in the size.
-  char+           \ Forward to locals sub-section.
+  dup u32@        \ Read code vec
+  drop            \ Currently only one is supported
+  dup u32@        \ Read code size
+  drop            \ We are not interested in the function size
+  dup u32@        \ Reading locals
+  drop            \ Assuming no locals TODO
+  dup c@              \ Reading first instruction
   case
     i32.const of wasm-compile-i32.const endof
     i32.add   of wasm-compile-i32.add endof
