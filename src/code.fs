e92ce2001f5e612fddf41dedec37a0c860ec5972
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2023-12-12 15:09:51 +0100

    compilation

--- src/code.fs
+++ src/code.fs
@@ -15,75 +15,109 @@ $6A constant i32.add
 \ Variable Instructions
 $20 constant local.get
 
+\ Block return types.
+$40 constant VOID
+
 : wasm-compile-i32.const ( addr1 -- addr2 )
-  char+       \ Forward to constant value.
-  dup i32@    \ Read value.
-  u-to-s compile-file 
+  char+                \ Skip op-code.
+  dup i32@             \ Read value.
+  u-to-s compile-file  \ Write constant to output.
   compile-cr
 ;
 
 : wasm-compile-i32.add ( addr1 -- addr2 )
-  char+
+  char+                 \ Skip op-code.
   s" add" compile-file 
   compile-cr
 ;
 
-\ : wasm-compile-br
-\ ;
+: wasm-compile-br
+  char+       \ FSkip op-code.
+  u32@        \ Read nesting level label.
+  number-generator swap -
+  { jmp }
+  s" depth block" compile-file 
+  jmp u-to-s compile-file 
+  s"  - " compile-file 
+  compile-cr          
 
-: wasm-compile-block ( block-type number-generator addr end-instruction-ptr -- addr2 )
+  \ Restoring the stack                   
+  block-type VOID = if
+    \ The block has no return values
+    s" 0 ?do drop loop" compile-file compile-cr
+  else
+    \ The block has one return value
+    s" 1- 0 ?do drop loop" compile-file compile-cr
+  endif 
+;
+
+: wasm-compile-block.end
+  char+                 \ Skip op-code.
+;
+
+: wasm-compile-local.get
+  char+                 \ Skip op-code.
+  char+
+;
+
+: wasm-compile-block.instr ( addr1 code-end -- addr2 ) 
+  { code-end }
+  char+                  \ Skip op-code.
+  dup c@ { block-type }  \ Read block type.                     
+  char+                  \ Forward.
+  \ TODO: sp@ sp! to read and store stack pointer.
+  \ TODO: store depth on return stack,
+  s" depth >r" write-ln        
+  block-type 
+  code-end 
+  wasm-compile-block 
+;
+
+: wasm-compile-block ( addr1 btype ngen code-end -- addr2 )
   recursive
-  { block-type number-generator end-instruction-ptr }
+  { block-type number-generator code-end }
   begin
     dup c@ ~~     \ Reading instruction
     case
       i32.const   of wasm-compile-i32.const endof
       i32.add     of wasm-compile-i32.add endof
       br          of 
-                  char+       \ Forward to constant value.
-                  u32@        \ Read label.
-                  number-generator swap -
-                  { jmp }
-                  s" depth block" compile-file 
-                  jmp u-to-s compile-file 
-                  s"  - " compile-file 
-                  compile-cr          
+                    char+       \ Forward to nesting level label.
+                    u32@        \ Read nesting level label.
+                    number-generator swap -
+                    { jmp }
+                    s" depth block" compile-file 
+                    jmp u-to-s compile-file 
+                    s"  - " compile-file 
+                    compile-cr          
 
-                  \ Restoring the stack
-                  
-                  block-type $40 = if
-                  \ The block has no return values
-
-                  s" 0 ?do drop loop" compile-file compile-cr
-
-                  else
-                  \ The block has one return value
-
-                  s" 1- 0 ?do drop loop" compile-file compile-cr
-
-                  endif 
-
+                    \ Restoring the stack                   
+                    block-type VOID = if
+                      \ The block has no return values
+                      s" 0 ?do drop loop" compile-file compile-cr
+                    else
+                      \ The block has one return value
+                      s" 1- 0 ?do drop loop" compile-file compile-cr
+                    endif 
                   endof
-      block.end   of char+ exit endof
-      local.get   of char+ char+ endof
+                  \ TODO: what does exit do?
+      block.end   of wasm-compile-block.end exit endof
+      local.get   of wasm-compile-local.get endof
       block.instr of 
-                  char+ \ Read instruction
-                  dup c@ \ Read blocktype 
-                  { block-type }
-                  char+ 
-
-                  s" depth { block" compile-file
-                  number-generator u-to-s compile-file 
-                  s"  } " compile-file
-                  compile-cr          
-
-                  block-type number-generator 1+ end-instruction-ptr wasm-compile-block 
-
+                    char+                  \ Skip op-code.
+                    dup c@ { block-type }  \ Read block type.                     
+                    char+ 
+                    s" depth { block" compile-file
+                    number-generator u-to-s compile-file 
+                    s"  } " compile-file
+                    compile-cr          
+                    block-type 
+                    number-generator 1+ 
+                    code-end 
+                    wasm-compile-block 
                   endof
     endcase
-
-  dup end-instruction-ptr >=
-  until
+  dup code-end >= until
 ;
 
 : wasm-compile-code-section ( addr1 -- addr2 )
@@ -91,19 +125,21 @@ $20 constant local.get
     s" Expecting code section" exception
     throw
   endif
-  char+           \ Forward to code block size.
-  dup u32@        \ Read size field.
-  drop            \ We are not intersted in the size.
-  dup u32@        \ Read code vec
-  drop            \ Currently only one is supported
-  dup u32@        \ Read code size
+  char+                 \ Forward to code block size.
+  dup u32@              \ Read size field.
+  drop                  \ We are not intersted in the size.
+  dup u32@              \ Read code vec.
+  drop                  \ Currently only one is supported.
+  dup u32@              \ Read code size.
+  \ over +                   \ Compute the end of the code block.
   { code-size }            
-  dup code-size + \ Compute end of code block
-  1-              \ Subtract one because it will be used as index
-  { end-instruction-ptr }
-  dup u32@                      \ Reading locals
-  { number-of-locals }          \ Assuming no locals TODO
-  number-of-locals +            \ Skipping locals because locals have always one byte size
-
-  $40 0 end-instruction-ptr wasm-compile-block    
+  dup code-size +       \ Compute end of code block.
+  1- { code-end }       \ Subtract one because it will be used as index.
+  \ dup u32@ +               \ Skip locals.
+  dup u32@              \ Reading locals.
+  \ TODO: support for locals.
+  { number-of-locals }  \ Assuming no locals.
+  number-of-locals +    \ Skipping locals because locals have always one byte size.
+  VOID 0 code-end       \ Push block type, initial counter and end of code address.
+  wasm-compile-block    \ Compile instructions.
 ;
--- src/utils.fs
+++ src/utils.fs
@@ -4,9 +4,25 @@ s" ./output.fs" r/w create-file throw to fd-out
 : u-to-s ( u -- c-addr u ) 0 <# #s #> ;
 
 : compile-file ( addr u -- )
-    fd-out write-file throw
+  fd-out write-file throw
 ;
 
 : compile-cr ( -- ) 
-    s"  " fd-out write-line throw
+  s"  " fd-out write-line throw
+;
+
+: write ( str -- )
+  fd-out write-file throw
+;
+
+: write-ln ( str -- )
+  fd-out write-line throw
+;
+
+: write-num ( u -- )
+  u-to-s fd-out write-file throw
+;
+
+: write-cr ( -- ) 
+  s"  " fd-out write-line throw
 ;
