43c5cbb3e14dc3520f16904630a927e7c7782b21
Mathias Hörtnagl <mathias.hoertnagl@gmail.com>
2023-12-17 11:48:21 +0100

    one level value restore

--- wasm-runtime.fs
+++ wasm-runtime.fs
@@ -12,11 +12,45 @@ RP@ CONSTANT RBIAS
   .s 
 ; immediate
 
-create spos   256 cells allot  \ Stack of stack pointer positions.
-create numret 256 cells allot  \ Stack of number of returned block items.
-create wasm-rp       1 cells allot  \ Pointer.
+create wasm-sp 256 cells allot  \ Stack of stack pointer positions.
+create wasm-rt 256 cells allot  \ Stack of number of returned block items.
+create wasm-rp   1 cells allot  \ Pointer.
 
 0 wasm-rp !  \ Initialize pointer.
+
+: wasm-store-sp
+  sp@ wasm-sp !
+;
+
+: wasm-store-rt
+  wasm-rt !
+;
+
+: wasm-rp++ 
+  wasm-rp @ 1+ wasm-rp !
+;
+
+: wasm-rp-- 
+  wasm-rp @ 1- wasm-rp !
+;
+
+: wasm-sp@
+  wasm-sp wasm-rp @ 1- cells + @
+;
+
+: wasm-rt@
+  wasm-rt wasm-rp @ 1- cells + @
+;
+
+: wasm-restore-stack
+  wasm-rt@ 0= if
+    wasm-sp@ sp!
+  else
+    >r
+    wasm-sp@ sp!
+    r>
+  endif
+;
 
 : cs-swap ( d0/o0 d1/o1 -- d1/o1 d0/o0 )
   \g Swaps the top two control stack frames.
@@ -40,12 +74,9 @@ create wasm-rp       1 cells allot  \ Pointer.
   \g   +-> then    |
   \g               +-> wasm-end
   \
-  postpone r>     \ Fetch return address.
-  postpone swap   \ Bring number of returned stack items to TOS.
-  postpone sp@    \ Get the current stack pointer.
-  postpone >r     \ Store the current stack pointer.
-  postpone >r     \ Store the number of returned stack items.
-  postpone >r     \ Restore return address.
+  postpone wasm-store-rt
+  postpone wasm-store-sp
+  postpone wasm-rp++
   postpone ahead  \ On block entry jump to then.
   postpone begin  \ Jump target for branches inside the block.
   cs-swap         \ Put block entry origin on top.
@@ -64,50 +95,9 @@ create wasm-rp       1 cells allot  \ Pointer.
   \g           |
   \g           +-> (don't care)
   \
-    postpone numret postpone !
-    postpone sp@ postpone spos postpone ! 
-    postpone wasm-rp postpone @ postpone 1+ postpone wasm-rp postpone !
-
-    \ postpone numret postpone @ postpone .
-    \ postpone spos postpone @ postpone .
-    \ wasm-rp 1 cells dump
-  \ postpone numret postpone !
-  \ postpone @sp postpone spos postpone !
-  \ [ 1 ] postpone wasm-rp postpone @  
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone r>     \ Fetch return address.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone swap   \ Bring number of returned stack items to TOS.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone sp@    \ Get the current stack pointer.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone >r     \ Store the current stack pointer.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone >r     \ Store the number of returned stack items.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
-  \ postpone >r     \ Restore return address.
-  \ postpone .s
-  \ postpone cr
-  \ postpone .RETURNSTACK 
-  \ postpone cr
+  postpone wasm-store-rt
+  postpone wasm-store-sp
+  postpone wasm-rp++
   postpone begin  \ Jump target for branches inside the block.
   0 0 0 dest      \ Phony control flow stack item.
 ; immediate
@@ -121,25 +111,7 @@ create wasm-rp       1 cells allot  \ Pointer.
                     \ on the control flow stack. Pick the dest-orig 
                     \ frame pair according to nesting level. Then 
                     \ take the dest part of the pair.
-  postpone spos postpone wasm-rp postpone @ postpone 1- postpone cells postpone + postpone @ postpone sp!
-  \ numret wasm-rp @ 1- cells + @ 0= if
-  \ spos wasm-rp @ 1- cells + @
-  \ else
-  \ endif
-  \ postpone r>       \ Fetch return address.
-  \ postpone r>       \ Load the number of stack items to return.
-  \ postpone 0=
-  \ postpone if 
-  \ postpone r>       \ Load the original stack pointer.
-  \ postpone sp!      \ Restore original stack position.
-  \ postpone else
-  \ postpone r>       \ Load the original stack pointer.
-  \ postpone swap     \ Swap returned item to TOS.
-  \ postpone >r       \ Stash returned item.
-  \ postpone sp!      \ Restore original stack position.
-  \ postpone r>       \ Restore returned item.
-  \ postpone endif  
-  \ postpone >r       \ Restore return address.
+  postpone wasm-restore-stack
   postpone again    \ Jump to the start of the block or loop.
 ; immediate
 
@@ -151,20 +123,7 @@ create wasm-rp       1 cells allot  \ Pointer.
                     \ on the control flow stack. Pick the dest-orig 
                     \ frame pair according to nesting level. Then 
                     \ take the dest part of the pair.
-  postpone r>       \ Fetch return address.
-  postpone r>       \ Load the number of stack items to return.
-  postpone 0=
-  postpone if 
-  postpone r>       \ Load the original stack pointer.
-  postpone sp!      \ Restore original stack position.
-  postpone else
-  postpone r>       \ Load the original stack pointer.
-  postpone swap     \ Swap returned item to TOS.
-  postpone >r       \ Stash returned item.
-  postpone sp!      \ Restore original stack position.
-  postpone r>       \ Restore returned item.
-  postpone endif  
-  postpone >r       \ Restore return address.
+  postpone wasm-restore-stack
   postpone again    \ Jump to the start of the block or loop.
   postpone endif
 ; immediate
@@ -179,15 +138,7 @@ create wasm-rp       1 cells allot  \ Pointer.
     postpone then   \ Jump target for the WASM block.
   endif
   cs-drop           \ Remove destinatin control frame.
-    postpone wasm-rp postpone @ postpone 1- postpone wasm-rp postpone !
-
-    \ postpone wasm-rp postpone @ postpone .
-  \ postpone r>       \ Fetch return address.
-  \ postpone r>       \ Load the number of returned stack items.
-  \ postpone drop     \ Drop them.
-  \ postpone r>       \ Load the original stack pointer.
-  \ postpone drop     \ Drop it.
-  \ postpone >r       \ Restore return address.
+  postpone wasm-rp--
 ; immediate
 
 \ : main
@@ -207,8 +158,11 @@ create wasm-rp       1 cells allot  \ Pointer.
 : main
   \ 0 wasm-loop
     s" outer loop " type cr
-    0 wasm-loop
+    1 wasm-loop
+      .s
       s" inner loop " type cr
+      41
+      42
       [ 0 ] wasm-br
       s" inner loop - !!! " type cr
     wasm-end
